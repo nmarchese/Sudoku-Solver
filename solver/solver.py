5324444cbaefdba57aae10f82bb0828b94441c7a
Nicholas Marchese <nmarchese@gmail.com>
2020-05-24 16:18:48 +1000

    add new solve method (and suporting methods) that uses notes rather than blind brute force to find a solution, add tests for new solve method and new supporting methods, slight refactors to existing code, update commenting/documentation.

--- solver/solver.py
+++ solver/solver.py
@@ -1,3 +1,6 @@
+from time import time
+from collections import OrderedDict
+
 hard_board = [
   [0, 0, 0, 1, 0, 7, 3, 0, 0],
   [0, 0, 1, 0, 0, 5, 0, 0, 0],
@@ -31,52 +34,108 @@ wrong_board = [
   [0, 0, 0, 3, 0, 0, 9, 0, 0],
   [0, 0, 2, 7, 0, 9, 0, 0, 0]
 ]
+board_len = 9
 
 def print_board(board):
-  for i in range(len(board)):
+  """Pretty print board with dividing lines and empty cells (0) converted to a blank space"""
+  for i in range(board_len):
     if i != 0 and i % 3 == 0:
+      # print dividing line every 3 rows
       print("------+-------+------")
-    for j in range(len(board[i])):
+    for j in range(board_len):
       if j != 0 and j % 3 == 0:
+        # print divider every 3 cells
         print("| ", end="")
       print(board[i][j] if board[i][j] != 0 else " ", end=" ")
     print()
 
+def solve_timed(board, solve_method, timer):
+  """Solve board using given solve_method and record ammount of time taken to complete"""
+  start_time = time()
+  solved = solve_method(board)
+  finish_time = time()
+  timer.append(finish_time - start_time)
+  return solved
+
 def solve(board):
+  """Solve board using basic backtracking algorithm"""
   next = find_next_empty(board)
   if next:
     # try all numbers for next position
     for num in range(1,10):
       if check_valid(num, next, board):
+        # number is valid, set to position
         board[next[0]][next[1]] = num
+        # recursive call to solve next position
         if solve(board):
           return True
 
+        # couldn't find valid number for next position
         # backtrack
         board[next[0]][next[1]] = 0
+    
+    # board unsolvable
     return False
   else:
+    # every position filled, board solved
+    return True
+
+def solve_with_note_sorting_start(board):
+  """Make initial call to recursive method with a new notes dict from get_notes_dict"""
+  return solve_with_note_sorting(board, get_notes_dict(board))
+
+def solve_with_note_sorting(board, notes_dict):
+  """
+  solve board using backtracking algorithm that goes through sorted positions
+  starting from the position with the fewest possible options
+  """
+
+  # get item with fewest notes in notes_dict
+  next = get_pos_with_fewest_notes(notes_dict)
+
+  if next:
+    # try each number in next position's notes
+    for num in notes_dict[next]:
+      if remove_notes(num, next, notes_dict):
+        # move results in notes_dict remaining valid, set position to num
+        board[next[0]][next[1]] = num
+        
+        # recursive call to solve next position
+        if solve_with_note_sorting(board, notes_dict):
+          return True
+
+        # couldn't find valid number for next position
+        # backtrack
+        board[next[0]][next[1]] = 0
+        add_notes(next, notes_dict, board)
+
+    # board unsolvable
+    return False
+  else:
+    # no more notes, all positions filled
     return True
 
 def find_next_empty(board):
-  for y in range(len(board)):
-    for x in range(len(board[y])):
-      if board[y][x] == 0:
+  """Finds the next empty cell each row, left to right, top to bottom"""
+  for y in range(board_len):
+    for x in range(board_len):
+      if not board[y][x]:
         return (y, x)
 
   return None
 
 def check_valid(num, pos, board):
+  """Check if number (num) provided for position (pos) is a valid/legal sudoko move"""
   y_pos = pos[0]
   x_pos = pos[1]
 
   # check row:
-  for x in range(len(board[y_pos])):
+  for x in range(board_len):
     if num == board[y_pos][x]:
       return False
 
   # check column:
-  for y in range(len(board)):
+  for y in range(board_len):
     if num == board[y][x_pos]:
       return False
   
@@ -90,12 +149,150 @@ def check_valid(num, pos, board):
 
   return True
 
-def run():
-  run_board = hard_board
-  if solve(run_board):
-    print_board(run_board)
+def get_notes(pos, board):
+  """get list of all posible valid numbers for given position (pos) and board"""
+  notes = []
+  if not board[pos[0]][pos[1]]:
+    for num in range(1,10):
+      if check_valid(num, pos, board):
+        notes.append(num)
+  
+  return notes
+    
+def get_notes_dict(board):
+  """get dict of notes for every empty coordinate on board (position : notes_list)"""
+  notes_dict = {}
+  for y in range(board_len):
+    for x in range(board_len):
+      pos = (y,x)
+      notes_dict[pos] = get_notes(pos, board)
+  
+  return notes_dict
+
+def remove_notes(num, pos, notes_dict):
+  """
+  Remove number (num) from notes in notes_dict for each position in the given
+  position's (pos) row, column, and box
+  """
+
+  y_pos = pos[0]
+  x_pos = pos[1]
+  
+  # remove notes for pos
+  notes_dict[pos] = []
+
+  # remove notes for num in row and column
+  for xy in range(board_len):
+    y_pos_notes = notes_dict[(y_pos, xy)]
+    x_pos_notes = notes_dict[(xy, x_pos)]
+    if x_pos_notes.count(num):
+      # num exists in pos' notes, remove it
+      x_pos_notes.remove(num)
+      if not x_pos_notes:
+        # no notes remain for unfilled pos, notes_dict no longer valid
+        return False
+    if y_pos_notes.count(num):
+      # num exists in pos' notes, remove it
+      y_pos_notes.remove(num)
+      if not y_pos_notes:
+        # no notes remain for unfilled pos, notes_dict no longer valid
+        return False
+
+  # remove notes for num in box
+  y_box_root = y_pos - (y_pos % 3)
+  x_box_root = x_pos - (x_pos % 3)
+  for y in range(y_box_root, y_box_root + 3):
+    for x in range(x_box_root, x_box_root + 3):
+      pos_notes = notes_dict[(y, x)]
+      if pos_notes.count(num):
+        # num exists in pos' notes, remove it
+        pos_notes.remove(num)
+        if not pos_notes:
+          # no notes remain for unfilled pos, notes_dict no longer valid
+          return False
+  
+  # removed all notes and all effected positions still have at least one note
+  return True
+
+def add_notes(pos, notes_dict, board):
+  """
+  Add notes back (get new list of correct notes based on current board) for
+  every position in the provided position's (pos) row, column, and box
+  """
+
+  y_pos = pos[0]
+  x_pos = pos[1]
+  
+  # add notes for pos
+  notes_dict[pos] = get_notes(pos, board)
+
+  # add notes for pos row and column
+  for xy in range(board_len):
+    if not board[y_pos][xy]:
+      # position on board is empty, add notes
+      notes_dict[(y_pos, xy)] = get_notes((y_pos, xy), board)
+    if not board[xy][x_pos]:
+      # position on board is empty, add notes
+      notes_dict[(xy, x_pos)] = get_notes((xy, x_pos), board)
+  
+  # add notes for pos box
+  y_box_root = y_pos - (y_pos % 3)
+  x_box_root = x_pos - (x_pos % 3)
+  for y in range(y_box_root, y_box_root + 3):
+    for x in range(x_box_root, x_box_root + 3):
+      if not board[y][x]:
+        # position on board is empty, add notes
+        notes_dict[(y, x)] = get_notes((y, x), board)
+
+def get_pos_with_fewest_notes(notes_dict):
+  """Get the position with the fewest current notes"""
+  # sort notes_dict fewest notes to most notes
+  ordered_notes_dict = OrderedDict(sorted(notes_dict.items(), key=lambda t: len(t[1])))
+  for pos in ordered_notes_dict:
+    # only return first position that has non-empty notes list
+    if ordered_notes_dict[pos]:
+      return pos
+  # no notes remain
+  return None
+
+def run(board):
+  """ Run both solve methods with a timer using provided board"""
+
+  print()
+  print("Solving provided Sudoku board:")
+  print_board(board)
+
+  print()
+  print("Solving board with basic backtracking solve method:")
+  board_1 = [row[:] for row in board]
+  timer_1 = []
+  if solve_timed(board_1, solve, timer_1):
+    print_board(board_1)
   else:
-    print("no solution?")
+    print("*** Couldn't find a solution ***")
+  print("Finished in:", *timer_1)
+
+  print()
+
+  print("Solving board with notes driven backtracking solve method:")
+  board_2 = [row[:] for row in board]
+  timer_2 = []
+  if solve_timed(board_2, solve_with_note_sorting_start, timer_2):
+    print_board(board_2)
+  else:
+    print("*** Couldn't find a solution ***")
+  print("Finished in:", *timer_2)
+
+  print()
+  if board_1 == board_2:
+    print("Notes driven solve method was", "%.2f" % (timer_1[0] / timer_2[0]), "times faster")
+  else:
+    print("Solve methods found different solutions...")
+
+  print()
 
 if __name__ == '__main__':
-  run()
+  run(wrong_board)
+  run(hard_board)
+  # run(expert_board) 
+  # #TODO: notes driven solve can't complete expert board...
--- test_solver.py
+++ test_solver.py
@@ -68,22 +68,189 @@ test_board_unsolvable = [
   [0, 0, 0, 3, 0, 0, 9, 0, 0],
   [0, 0, 2, 7, 0, 9, 0, 0, 0]
 ]
+test_board_not_started_notes_dict = {
+  (0,0): [2,4,5,9],
+  (0,1): [4,5,6],
+  (0,2): [5,6],
+  (0,3): [],
+  (0,4): [4,6,8],
+  (0,5): [],
+  (0,6): [],
+  (0,7): [2,4,5,6,9],
+  (0,8): [2,4,5,8,9],
+  (1,0): [2,4,7,9],
+  (1,1): [3,4,6,7],
+  (1,2): [],
+  (1,3): [4,6,8],
+  (1,4): [3,4,6,8],
+  (1,5): [],
+  (1,6): [2,6,8],
+  (1,7): [2,4,6,9],
+  (1,8): [2,4,8,9],
+  (2,0): [],
+  (2,1): [3,4,5,6],
+  (2,2): [3,5,6],
+  (2,3): [],
+  (2,4): [],
+  (2,5): [3,4,6],
+  (2,6): [1,5,6],
+  (2,7): [],
+  (2,8): [4,5],
+  (3,0): [],
+  (3,1): [],
+  (3,2): [5,7,8],
+  (3,3): [5,8,9],
+  (3,4): [3,7,8],
+  (3,5): [3],
+  (3,6): [],
+  (3,7): [3,5,9],
+  (3,8): [],
+  (4,0): [1,5],
+  (4,1): [1,5,8],
+  (4,2): [],
+  (4,3): [4,5,6,8],
+  (4,4): [1,2,3,4,6,8],
+  (4,5): [1,2,3,4,6],
+  (4,6): [],
+  (4,7): [2,3,5],
+  (4,8): [2,5],
+  (5,0): [],
+  (5,1): [1,5,7],
+  (5,2): [],
+  (5,3): [5,9],
+  (5,4): [1,2,7],
+  (5,5): [1,2],
+  (5,6): [2,5],
+  (5,7): [],
+  (5,8): [],
+  (6,0): [1,4,7],
+  (6,1): [],
+  (6,2): [6,7],
+  (6,3): [4,6],
+  (6,4): [],
+  (6,5): [],
+  (6,6): [1,2,6],
+  (6,7): [1,2,4,6],
+  (6,8): [],
+  (7,0): [1,4,5,7],
+  (7,1): [1,4,5,6,7,8],
+  (7,2): [5,6,7,8],
+  (7,3): [],
+  (7,4): [1,2,4,6],
+  (7,5): [1,2,4,6],
+  (7,6): [],
+  (7,7): [1,2,4,5,6],
+  (7,8): [2,4,5,7,8],
+  (8,0): [1,4,5],
+  (8,1): [1,3,4,5,6,8],
+  (8,2): [],
+  (8,3): [],
+  (8,4): [1,4,6],
+  (8,5): [],
+  (8,6): [1,5,6,8],
+  (8,7): [1,4,5,6],
+  (8,8): [4,5,8],
+}
+test_board_not_started_notes_dict_after_first_move = {
+  (0,0): [2,4,5,9],
+  (0,1): [4,5,6],
+  (0,2): [5,6],
+  (0,3): [],
+  (0,4): [4,6,8],
+  (0,5): [],
+  (0,6): [],
+  (0,7): [2,4,5,6,9],
+  (0,8): [2,4,5,8,9],
+  (1,0): [2,4,7,9],
+  (1,1): [3,4,6,7],
+  (1,2): [],
+  (1,3): [4,6,8],
+  (1,4): [3,4,6,8],
+  (1,5): [],
+  (1,6): [2,6,8],
+  (1,7): [2,4,6,9],
+  (1,8): [2,4,8,9],
+  (2,0): [],
+  (2,1): [3,4,5,6],
+  (2,2): [3,5,6],
+  (2,3): [],
+  (2,4): [],
+  (2,5): [4,6],
+  (2,6): [1,5,6],
+  (2,7): [],
+  (2,8): [4,5],
+  (3,0): [],
+  (3,1): [],
+  (3,2): [5,7,8],
+  (3,3): [5,8,9],
+  (3,4): [7,8],
+  (3,5): [],
+  (3,6): [],
+  (3,7): [5,9],
+  (3,8): [],
+  (4,0): [1,5],
+  (4,1): [1,5,8],
+  (4,2): [],
+  (4,3): [4,5,6,8],
+  (4,4): [1,2,4,6,8],
+  (4,5): [1,2,4,6],
+  (4,6): [],
+  (4,7): [2,3,5],
+  (4,8): [2,5],
+  (5,0): [],
+  (5,1): [1,5,7],
+  (5,2): [],
+  (5,3): [5,9],
+  (5,4): [1,2,7],
+  (5,5): [1,2],
+  (5,6): [2,5],
+  (5,7): [],
+  (5,8): [],
+  (6,0): [1,4,7],
+  (6,1): [],
+  (6,2): [6,7],
+  (6,3): [4,6],
+  (6,4): [],
+  (6,5): [],
+  (6,6): [1,2,6],
+  (6,7): [1,2,4,6],
+  (6,8): [],
+  (7,0): [1,4,5,7],
+  (7,1): [1,4,5,6,7,8],
+  (7,2): [5,6,7,8],
+  (7,3): [],
+  (7,4): [1,2,4,6],
+  (7,5): [1,2,4,6],
+  (7,6): [],
+  (7,7): [1,2,4,5,6],
+  (7,8): [2,4,5,7,8],
+  (8,0): [1,4,5],
+  (8,1): [1,3,4,5,6,8],
+  (8,2): [],
+  (8,3): [],
+  (8,4): [1,4,6],
+  (8,5): [],
+  (8,6): [1,5,6,8],
+  (8,7): [1,4,5,6],
+  (8,8): [4,5,8],
+}
 
 class TestSolver(unittest.TestCase):
   def test_find_next_empty(self):
     """
     Test that it finds the next empty cell
     """
-    board = test_board_partial_complete_1
+    board = [row[:] for row in test_board_partial_complete_1]
     result = solver.find_next_empty(board)
-    self.assertEqual(result, (1, 3))
+    expected = (1, 3)
+    self.assertEqual(result, expected)
 
   def test_check_valid_returns_true_when_valid(self):
     """
     Test that check valid returns true when a valid number is
     provided for the given position and current board.
     """
-    board = test_board_partial_complete_1
+    board = [row[:] for row in test_board_partial_complete_1]
     position = (1, 3)
     number = 6
     result = solver.check_valid(number, position, board)
@@ -94,7 +261,7 @@ class TestSolver(unittest.TestCase):
     Test that check valid returns false when number provided is
     invalid because same number exists in same row.
     """
-    board = test_board_not_started
+    board = [row[:] for row in test_board_not_started]
     position = (0, 0)
     number = 7
     result = solver.check_valid(number, position, board)
@@ -105,7 +272,7 @@ class TestSolver(unittest.TestCase):
     Test that check valid returns false when number provided is
     invalid because same number exists in same column.
     """
-    board = test_board_not_started
+    board = [row[:] for row in test_board_not_started]
     position = (0, 0)
     number = 6
     result = solver.check_valid(number, position, board)
@@ -116,7 +283,7 @@ class TestSolver(unittest.TestCase):
     Test that check valid returns false when number provided is
     invalid because same number exists in same box.
     """
-    board = test_board_partial_complete_2
+    board = [row[:] for row in test_board_partial_complete_2]
     position = (0, 1)
     number = 8
     result = solver.check_valid(number, position, board)
@@ -127,7 +294,7 @@ class TestSolver(unittest.TestCase):
     Test that solve returns true and modifies board to correct
     completed status when a solvable board is provided.
     """
-    board = test_board_not_started
+    board = [row[:] for row in test_board_not_started]
     result = solver.solve(board)
     self.assertTrue(result)
     self.assertEqual(board, test_board_solved)
@@ -137,8 +304,100 @@ class TestSolver(unittest.TestCase):
     Test that solve returns false and when an unsolvable
     board is provided.
     """
-    board = test_board_unsolvable
+    board = [row[:] for row in test_board_unsolvable]
     result = solver.solve(board)
+    self.assertFalse(result)
+
+  def test_get_notes_returns_correct_notes(self):
+    """
+    Test that get_notes returns a list of all posible/valid
+    numbers for the given position and board
+    """
+    board = [row[:] for row in test_board_not_started]
+    pos = (0, 0)
+    result = solver.get_notes(pos, board)
+    expected = [2, 4, 5, 9]
+    self.assertEqual(result, expected)
+
+  def test_get_notes_returns_empty_list_for_completed_pos(self):
+    """
+    Test that get_notes returns an empty list when position is
+    already filled
+    """
+    board = [row[:] for row in test_board_not_started]
+    pos = (0, 3)
+    result = solver.get_notes(pos, board)
+    expected = []
+    self.assertEqual(result, expected)
+
+  def test_get_notes_dict_returns_correct_notes_for_all_pos(self):
+    """
+    Test that get_notes_dict returns dictionary of all positions
+    to notes with each notes list containing correct notes
+    """
+    board = [row[:] for row in test_board_not_started]
+    result = solver.get_notes_dict(board)
+    expected = test_board_not_started_notes_dict
+    self.assertEqual(result, expected)
+
+  def test_remove_notes_removes_notes_correctly(self):
+    """
+    Test that remove_notes removes all notes for position (pos)
+    and removes the notes for the number (num) provided in that
+    position's row, column, and box
+    """
+    notes_dict = test_board_not_started_notes_dict.copy()
+    num = 3
+    y_pos = 3
+    x_pos = 5
+    bool_result = solver.remove_notes(num, (y_pos, x_pos), notes_dict)
+    expected = test_board_not_started_notes_dict_after_first_move
+    self.assertTrue(bool_result)
+    self.assertEqual(notes_dict, expected)
+
+  def test_remove_notes_returns_false_when_resulting_in_invalid_notes(self):
+    """
+    Test that remove_notes returns false when the removal of notes results
+    in an un-filled position being left with no notes. i.e. remove_notes
+    removes the last note from a position other than the position passed in
+    """
+    notes_dict = test_board_not_started_notes_dict.copy()
+    num = 3
+    y_pos = 3
+    x_pos = 4
+    bool_result = solver.remove_notes(num, (y_pos, x_pos), notes_dict)
+    self.assertFalse(bool_result)
+
+  def test_add_notes_adds_notes_back_correctly(self):
+    """
+    Test that add_notes adds all notes back to position (pos)
+    and adds number (num) to notes lists for that position's
+    row, column, and box
+    """
+    notes_dict = test_board_not_started_notes_dict_after_first_move.copy()
+    y_pos = 3
+    x_pos = 5
+    solver.add_notes((y_pos, x_pos), notes_dict, test_board_not_started)
+    expected = test_board_not_started_notes_dict
+    self.assertEqual(notes_dict, expected)
+
+  def test_solve_with_note_sorting_returns_true_and_completes_board(self):
+    """
+    Test that solve_with_note_sorting returns true and modifies board
+    to correct completed status when a solvable board is provided.
+    """
+    board = [row[:] for row in test_board_not_started]
+    result = solver.solve_with_note_sorting_start(board)
+    self.assertTrue(result)
+    self.assertEqual(board, test_board_solved)
+
+  def test_solve_with_note_sorting_returns_false_for_unsolvable_board(self):
+    """
+    Test that solve_with_note_sorting returns false and when an
+    unsolvable board is provided.
+    """
+    board = [row[:] for row in test_board_unsolvable]
+    result = solver.solve_with_note_sorting_start(board)
     self.assertFalse(result)
 
 if __name__ == '__main__':
